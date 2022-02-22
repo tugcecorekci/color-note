cdb5336a2970eb624b9c82b2026756730a96f35e
tugcecorekci <tugcecorekcioglu@gmail.com>
2022-02-22 22:29:50 +0100

    Added color objects, note array and note search feature.

--- app.js
+++ app.js
@@ -1,86 +1,153 @@
 //dropdown menu
-const colors = ["#D5FFF3", "#00916E", "#623CEA", "#DDDDDF"]
+const colors = [
+    {
+        colorname: "blue",
+        colorcode: "#80DEEA"
+    },
+    {
+        colorname: "purple",
+        colorcode: "#BA68C8"
+    },
+    {
+        colorname: "yellow",
+        colorcode: "#FFF176"
+    },
+    {
+        colorname: "green",
+        colorcode: "#C7F0BD"
+    }
+]
+
 const drop = document.querySelector('select')
+drop.setAttribute("required", "true")
 drop.appendChild(new Option("select", "0"))
 
 function colorOptions(a) {
-    for (color of colors) {
+    for (c of colors) {
         const option = document.createElement("option");
-        option.setAttribute("value", color);
-        option.text = color
+        option.setAttribute("value", c.colorcode);
+        option.text = c.colorname
         a.appendChild(option)
     }
 }
 
+
 colorOptions(drop)
 
-//submit button - add event
+const notes = []
+const noteSection = document.querySelector('.noteSection')
+
 const submitBtn = document.querySelector('#submitBtn')
 submitBtn.addEventListener('click', clickHandler)
 
-//submit button event func
 function clickHandler(e) {
-    e.preventDefault()
-    addNewNote()
-    document.querySelector('#newNote').value = ""
-    drop.value = 0
-    tglForm.style.display = "none"
+    if (drop.value != 0 && document.querySelector('#newNote').value != 0) {
+        e.preventDefault()
+        removeNotes()
+        newObj()
+        addNotes()
+        tglForm.style.display = "none"
+        document.querySelector('#newNote').value = ""
+        drop.value = 0
+    }
 }
 
-//submit button event func - declare
-function addNewNote() {
-    const newNote = document.createElement('p')
-    newNote.setAttribute("class", "oneNote")
-    appendInputText(newNote)
-    appendDropMenu(newNote)
-    appendDeleteBtn(newNote)
-    const noteSection = document.querySelector('.noteSection')
+function removeNotes() {
+    while (noteSection.firstChild) {
+        noteSection.removeChild(noteSection.lastChild);
+    }
+}
+
+function newObj() {
+    const newNoteInput = document.querySelector('#newNote').value
+    let obj = {}
+    obj["name"] = newNoteInput
+    obj["backgrounColor"] = drop.value
+    notes.push(obj)
+}
+
+function addNotes() {
+    for (let i = 0; i < notes.length; i++) {
+        const newNote = document.createElement('p')
+        newNote.setAttribute("class", "oneNote")
+        newNote.setAttribute("id", `pnumber${i + 1}`)
+        createNote(i, newNote)
+        createSelect(i, newNote)
+        createBtn(i, newNote)
+    }
+}
+
+function createNote(i, newNote) {
+    const div1 = document.createElement("div")
+    div1.setAttribute("class", "div1")
+    // div1.setAttribute("contentEditable", "true")
+    const noteText = document.createElement("input")
+    noteText.setAttribute("type", "text")
+    noteText.setAttribute("id", `number${i + 1}`)
+    noteText.value = notes[i]["name"]
+    div1.append(noteText)
+    newNote.append(div1)
+    noteText.addEventListener('change', function () {
+        notes[i]["name"] = noteText.value
+    })
+}
+
+function createSelect(i, newNote) {
+    const div2 = document.createElement('div')
+    div2.setAttribute("class", "div2")
+    const newDrop = document.createElement('select')
+    colorOptions(newDrop)
+    div2.append(newDrop)
+    newNote.append(div2)
+    newDrop.value = notes[i]["backgrounColor"]
+    newNote.style.backgroundColor = notes[i]["backgrounColor"]
+    newDrop.addEventListener('change', function () {
+        newNote.style.backgroundColor = newDrop.value
+        notes[i]["backgrounColor"] = newDrop.value
+    })
+}
+
+function createBtn(i, newNote) {
+    const newBtn = document.createElement('button')
+    const newDiv3 = document.createElement('div')
+    newDiv3.setAttribute("class", "div3")
+    newBtn.classList.add("newNoteDelete")
+    newBtn.innerHTML = "delete"
+    newBtn.addEventListener('click', function () {
+        newNote.remove()
+        notes.splice(i)
+    })
+    newDiv3.append(newBtn)
+    newNote.append(newDiv3)
     noteSection.append(newNote)
 }
 
-function appendInputText(newNote) {
-    const newNoteInput = document.querySelector('#newNote').value
-    const newDiv1 = document.createElement('div')
-    newDiv1.setAttribute("contentEditable", "true")
-    newDiv1.setAttribute("id", "div1")
-    newDiv1.append(newNoteInput)
-    newNote.append(newDiv1)
-}
-
-function appendDropMenu(newNote) {
-    const newDiv2 = document.createElement('div')
-    newDiv2.setAttribute("id", "div2")
-    const newDrop = document.createElement('select')
-    newDiv2.setAttribute("id", "selectId")
-    colorOptions(newDrop)
-    newDiv2.append(newDrop)
-    newNote.append(newDiv2)
-    newDrop.value = drop.value
-    newNote.style.backgroundColor = drop.value
-    newDrop.addEventListener('change', () => newNote.style.backgroundColor = newDrop.value)
-}
-
-function appendDeleteBtn(newNote) {
-    const newBtn = document.createElement('button')
-    const newDiv3 = document.createElement('div')
-    newDiv3.setAttribute("id", "div3")
-    newBtn.classList.add("newNoteDelete")
-    newBtn.innerHTML = "delete"
-    newBtn.addEventListener('click', () => newNote.remove())
-    newDiv3.append(newBtn)
-    newNote.append(newDiv3)
-}
 
 //toggle button
 const tglForm = document.getElementById('tglForm')
 const tglBtn = document.getElementById('tglBtn')
 tglBtn.onclick = function () {
-    if (tglForm.style.display !== "block") {
-        tglForm.style.display = "block"
-    } else {
+    if (tglForm.style.display !== "flex") {
+        tglForm.style.display = "flex"
+    }
+    else {
         tglForm.style.display = "none"
     }
 }
 
-//change-color
-const drop2 = document.querySelector('')
+// search function
+function searching() {
+    const search = document.getElementById('search')
+    const keys = search.value
+
+    for (let i = 0; i < notes.length; i++) {
+        let noted = document.getElementById(`number${i + 1}`)
+        let p = document.getElementById(`pnumber${i + 1}`)
+        if (noted.value.indexOf(keys) > -1) {
+            p.style.display = "flex"
+        } else {
+            p.style.display = "none"
+
+        }
+    } console.log(search.value)
+}
